9834e371667327b5f63ed1db458d6f1d8b50d935
JeongTaek Han <smart8612@gmail.com>
2023-06-16 22:16:55 +0900

    docs: write documentation to the NotificationRequest type #1

--- Sources/UserNotificationKit/NotificationRequest.swift
+++ Sources/UserNotificationKit/NotificationRequest.swift
@@ -8,13 +8,25 @@
 import Foundation
 import UserNotifications
 
-
+/// A type that contains the content and trigger of notification
+///
+/// NotificationRequest can be used to reserve a local notification
+/// by requesting to the user notification center controller.
 public protocol NotificationRequest {
     
+    /// The unique identifier of the notification request.
     var id: String { get }
+    
+    /// The category of the notification request.
     var category: NotificationCategory? { get }
+    
+    /// The content of the notification request.
     var content: UNNotificationContent { get }
+    
+    /// The trigger of the notification request.
     var trigger: UNNotificationTrigger { get }
+    
+    /// The encapsulated reqeust of the notification request.
     var request: UNNotificationRequest { get }
     
 }
